8323bbe5e8e94bde069eb97cc3d54bdda13834f3
slavislavi <slavislavi1986@gmail.com>
2023-07-12 14:18:26 +0300

    [main]: device-controller

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const sequelize = require('./db');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const models = require('./models/models');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandleMiddleware');
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 // errorHandler is always the last middleware
