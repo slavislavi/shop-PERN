87339ff2b91731ad35e225e1eead5ea91723bfc3
slavislavi <slavislavi1986@gmail.com>
2023-07-20 15:57:17 +0300

    [main]: navbar-buttons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { SHOP_ROUTE } from '../utils/constants';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/constants';
 import { getIsAuth } from '../store/selectors/userSelectors';
 
 const NavBar = () => {
     const isAuth = useSelector(getIsAuth);
+    const navigate = useNavigate();
+
+    const toAdminPanelHandler = () => navigate(ADMIN_ROUTE);
+    const logoutHandler = () => navigate(LOGIN_ROUTE);
 
     return (
         <Navbar bg="dark" data-bs-theme="dark">
@@ -17,8 +21,17 @@ const NavBar = () => {
                 <NavLink to={SHOP_ROUTE} style={{ color: "white" }}>Shop PERN</NavLink>
                 {isAuth ?
                     <Nav className="ml-auto" style={{ color: "white" }}>
-                        <Button variant="outline-light">Admin Panel</Button>
-                        <Button variant="outline-light" className="ms-2">Log Out</Button>
+                        <Button
+                            variant="outline-light"
+                            onClick={toAdminPanelHandler}
+                        >
+                            Admin Panel</Button>
+                        <Button
+                            variant="outline-light"
+                            className="ms-2"
+                            onClick={logoutHandler}
+                        >
+                            Log Out</Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{ color: "white" }}>
