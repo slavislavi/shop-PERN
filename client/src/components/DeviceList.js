fb22f612ce261185ec8ee3ecd969a1a10bae7e9a
slavislavi <slavislavi1986@gmail.com>
2023-07-20 13:47:24 +0300

    [main]: device-page-layout

--- client/src/components/DeviceList.js
+++ client/src/components/DeviceList.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getDevices } from '../store/selectors/deviceSelectors';
 import Row from 'react-bootstrap/Row';
 import DeviceItem from './DeviceItem';
 
 const DeviceList = () => {
     const devices = useSelector(getDevices);
-    const dispatch = useDispatch();
 
     return (
         <Row className="d-flex">
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap';
+import bigStar from '../assets/bigStar.png';
 
 const DevicePage = () => {
+    const device = { id: 1, name: 'Apple iphone 14 pro', price: 1400, rating: 5, img: 'https://img.5element.by/import/images/ut/goods/good_077c5dc7-06a8-11ee-bb93-005056012465/-1_600.jpg' };
+
+    const description = [
+        { id: 1, title: 'RAM', description: '6 Gb' },
+        { id: 2, title: 'Camera', description: '12 Mpx' },
+        { id: 3, title: 'Processor', description: 'M1 pro' },
+        { id: 4, title: 'Core', description: '10' },
+        { id: 5, title: 'Battery', description: '4200 mA' },
+    ];
+
     return (
-        <div>
-            DEVICE PAGE
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image src={device.img} width={300} height={300} />
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex justify-content-center align-items-center"
+                            style={{ background: `url(${bigStar}) no-repeat center center`, width: 240, height: 240, backgroundSize: "cover", fontSize: 64 }}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{ width: 300, height: 300, fontSize: 32, border: "5px solid lightgray" }}
+                    >
+                        <h3>{device.price} $</h3>
+                        <Button variant="outline-dark">Add to cart</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
