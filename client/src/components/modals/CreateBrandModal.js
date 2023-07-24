007cceee7fe12259c944eb2922aae1a549666c69
slavislavi <slavislavi1986@gmail.com>
2023-07-24 18:05:26 +0300

    [main]: set-up-brands

--- client/src/components/modals/CreateBrandModal.js
+++ client/src/components/modals/CreateBrandModal.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import { createBrand } from '../../http/deviceApi';
 
 const CreateBrandModal = ({ show, onHide }) => {
+    const [input, setInput] = useState('');
+
+    const addBrand = () => {
+        createBrand({ name: input }).then((data) => {
+            setInput('');
+            onHide();
+        });
+    };
+
+    const onChangeBrand = (e) => setInput(e.target.value);
+
     return (
         <Modal show={show} onHide={onHide} size="lg" centered>
             <Modal.Header closeButton>
@@ -12,13 +24,15 @@ const CreateBrandModal = ({ show, onHide }) => {
             <Modal.Body>
                 <Form>
                     <Form.Control
+                        value={input}
+                        onChange={onChangeBrand}
                         placeholder="Enter brand name..."
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>Close</Button>
-                <Button variant="outline-success" onClick={onHide}>Add</Button>
+                <Button variant="outline-success" onClick={addBrand}>Add</Button>
             </Modal.Footer>
         </Modal>
     );
