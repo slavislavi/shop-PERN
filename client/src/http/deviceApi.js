2e18ca3a5397df0787c6683e9096312fa2e28df5
slavislavi <slavislavi1986@gmail.com>
2023-07-24 17:27:45 +0300

    [main]: device-api-brands

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -9,3 +9,13 @@ export const fetchTypes = async () => {
     const { data } = await $host.get('api/type');
     return data;
 };
+
+export const createBrand = async (brand) => {
+    const { data } = await $authHost.post('api/brand', brand);
+    return data;
+};
+
+export const fetchBrands = async () => {
+    const { data } = await $host.get('api/brand');
+    return data;
+};
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TypeBar from '../components/TypeBar';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
+import { fetchBrands, fetchTypes } from '../http/deviceApi';
+import { deviceActions } from '../store/slices/deviceSlice';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchTypes().then((data) => dispatch(deviceActions.setTypes(data)));
+        fetchBrands().then((data) => dispatch(deviceActions.setBrands(data)));
+    }, []);
+
     return (
         <Container>
             <Row className="mt-2">
