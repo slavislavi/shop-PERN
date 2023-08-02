638d5a38273419799108aeacbcde02935e07b71e
slavislavi <slavislavi1986@gmail.com>
2023-08-02 20:23:43 +0300

    [main]: notification-custom-styles-fix

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,26 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { useDispatch } from 'react-redux';
 import { notificationActions } from '../store/slices/notificationSlice';
 
-const NOTIFICATION_DELAY = 3000;
+const NOTIFICATION_DELAY = 5000;
+const TRANSITION_DELAY = 200;
 
 const Notification = ({ message, variant }) => {
-    const [notificationMsg, setNotificationMsg] = useState('');
-    const [notificationClass, setNotificationClass] = useState('notification');
-    const containerEl = document.getElementById('notification-root');
     const notificationEl = useRef(null);
     const timeout = useRef();
     const dispatch = useDispatch();
-
-    const addVariantClass = () => {
-        if (variant === 'success') {
-            setNotificationClass('notification success');
-        }
-        if (variant === 'danger') {
-            setNotificationClass('notification danger');
-        }
-    };
 
     const removeNotification = () => {
         if (timeout.current) {
@@ -28,52 +17,36 @@ const Notification = ({ message, variant }) => {
         }
         if (notificationEl.current) {
             notificationEl.current.style.opacity = '0';
+            notificationEl.current.style.left = '-400px';
         }
         setTimeout(() => {
             dispatch(notificationActions.setNotification({ message: '', variant: '' }));
-        }, 200);
+        }, TRANSITION_DELAY);
     };
 
     useEffect(() => {
-        if (timeout.current) {
-            clearTimeout(timeout.current);
+        setTimeout(() => {
             if (notificationEl.current) {
-                notificationEl.current.style.opacity = '0';
+                notificationEl.current.style.opacity = '0.8';
+                notificationEl.current.style.left = 0;
+                timeout.current = setTimeout(() => {
+                    removeNotification();
+                }, NOTIFICATION_DELAY);
             }
-            setTimeout(() => {
-                setNotificationMsg(message);
-                addVariantClass();
-                setTimeout(() => {
-                    if (notificationEl.current) {
-                        notificationEl.current.style.opacity = '0.8';
-                        timeout.current = setTimeout(() => {
-                            removeNotification();
-                        }, NOTIFICATION_DELAY);
-                    }
-                }, 20);
-            }, 300);
-        } else {
-            setNotificationMsg(message);
-            addVariantClass();
-            setTimeout(() => {
-                if (notificationEl.current) {
-                    notificationEl.current.style.opacity = '0.8';
-                    timeout.current = setTimeout(() => {
-                        removeNotification();
-                    }, NOTIFICATION_DELAY);
-                }
-            }, 20);
-        }
+        }, 20);
+        return () => removeNotification();
     }, [message]);
 
     const output = (
-        <div className={notificationClass} ref={notificationEl}>
-            <div className="notification-text">{notificationMsg}</div>
-            <button className="notification-delete" onClick={removeNotification}>&#x2715;</button>
+        <div className="notification-container">
+            <div className={`notification ${variant}`} ref={notificationEl}>
+                <div className="notification-text">{message}</div>
+                <button className="notification-delete" onClick={removeNotification}>&#x2715;</button>
+            </div>
         </div>
     );
 
-    return containerEl ? createPortal(output, containerEl) : null;
+    return createPortal(output, document.getElementById('root'));
 };
 
 export default Notification;
