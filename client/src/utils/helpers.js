5d717930f8ec02992384af5cf26e99231f8ee83c
slavislavi <slavislavi1986@gmail.com>
2023-08-18 19:51:15 +0300

    [main]: use-scroll-and-trottle

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,5 +1,32 @@
-export const scrollToTop = () =>
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+import { useEffect, useState } from "react";
+
+export const scrollToTop = () => window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+
+const throttle = (callback, delay) => {
+    let time = Date.now();
+
+    return (...args) => {
+        if (time + delay - Date.now() < 0) {
+            callback(...args);
+            time = Date.now();
+        }
+    };
+};
+
+export const useScroll = () => {
+    const [scrollPosition, setScrollPosition] = useState(window.scrollY);
+
+    const updateScrollPosition = throttle(() => {
+        setScrollPosition(window.scrollY);
+    }, 100);
+
+    useEffect(() => {
+        window.addEventListener('scroll', updateScrollPosition);
+        return () => window.removeEventListener('scroll', updateScrollPosition);
+    }, [updateScrollPosition]);
+
+    return scrollPosition;
+};
