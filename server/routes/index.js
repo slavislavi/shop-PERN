27068379111a899282663231aa518af7d4a2bbc9
slavislavi <slavislavi1986@gmail.com>
2023-08-22 18:17:45 +0300

    [main]: update-express-router

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,12 @@ const brandRouter = require('./brandRouter');
 const userRouter = require('./userRouter');
 const typeRouter = require('./typeRouter');
 const deviceRouter = require('./deviceRouter');
+const basketRouter = require('./basketRouter');
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
 router.use('/brand', brandRouter);
 router.use('/device', deviceRouter);
+router.use('/basket', basketRouter);
 
 module.exports = router;
