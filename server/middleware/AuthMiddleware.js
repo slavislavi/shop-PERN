c7a51a6a934312bb81ad3e43e5e84292ace2f374
slavislavi <slavislavi1986@gmail.com>
2023-08-28 20:34:45 +0300

    [main]: fix-server-middlewares

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -8,12 +8,12 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
-            return res.status(401).json({ message: 'Access forbidden' });
+            return res.status(401).json({ message: 'Not authorized' });
         }
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.user = decoded;
         next();
     } catch (e) {
-        res.status(403).json({ message: 'Access forbidden' });
+        res.status(401).json({ message: 'Authorization Error' });
     }
 };
