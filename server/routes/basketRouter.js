70413a88b3fd6bcb5009a30edf059192c21268e8
slavislavi <slavislavi1986@gmail.com>
2023-08-30 14:52:14 +0300

    [main]: check-delete-from-basket-middleware

--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const basketController = require('../controllers/basketController');
+const checkDeleteDeviceFromBasket = require('../middleware/CheckDeleteDeviceFromBasket');
 
 router.get('/', authMiddleware, basketController.getByUser);
 router.post('/', authMiddleware, basketController.addToBasket);
-router.delete('/:id', authMiddleware, basketController.delete);
+router.delete('/:id', authMiddleware, checkDeleteDeviceFromBasket, basketController.delete);
 
 module.exports = router;
