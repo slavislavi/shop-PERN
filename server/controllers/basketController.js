d25075f1089e0f5c1249dc9c5217f996b07a38fc
slavislavi <slavislavi1986@gmail.com>
2023-08-30 19:42:30 +0300

    [main]: basket-controller

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -1,42 +1,66 @@
-const { BasketDevice, Device } = require('../models/models');
+const ApiError = require("../error/ApiError");
+const { Device, BasketDevice } = require("../models/models");
+const basketService = require("../service/basketService");
+const deviceController = require("./deviceController");
 
 class BasketController {
-    async addToBasket(req, res) {
-        const user = req.user;
-        const { deviceId } = req.body;
-
-        const isExist = await BasketDevice.findOne({ where: { id: user.id, deviceId } });
-
-        if (isExist) {
-            return res.status(400).send({ message: 'This product is already in the cart' });
-        }
-
-        const basketItem = await BasketDevice.create({
-            id: user.id,
-            deviceId: deviceId
-        });
-
-        return res.json(basketItem); // разобраться с id - напутано
-    }
-
-    async getByUser(req, res) {
-        const { id } = req.user;
-        const basketItems = await BasketDevice.findAll({
-            include: { model: Device },
-            where: { basketId: id }
-        });
-
-        return res.json(basketItems);
-    }
-
-    async delete(req, res) {
+    async addToBasket(req, res, next) {
         try {
             const user = req.user;
-            const { deviceId } = req.body; // req.params ? query ?
-            const deleted = await BasketDevice.destroy({ where: { id: user.id, id } });
-            return res.status(200).send('Successfuly removing item');
+            if (!user) {
+                throw ApiError.UnauthorizedError();
+            }
+
+            const { deviceId } = req.body;
+
+            const isExist = await BasketDevice.findOne({ where: { id: user.id, deviceId } });
+
+            if (isExist) {
+                return res.status(400).send({ message: 'This product is already in the cart' });
+            }
+
+            const basket = await BasketDevice.create({
+                basketId: user.id,
+                DeviceId: deviceId,
+            });
+
+            return res.json(basket);
         } catch (e) {
-            return res.status(500).send({ message: 'There was an error removing item' });
+            next(e);
+        }
+    }
+
+    async getByUser(req, res, next) {
+        try {
+            const { id } = req.user;
+
+            if (!id) {
+                throw ApiError.UnauthorizedError();
+            }
+
+            const basketItems = await BasketDevice.findAll({
+                include: { model: Device },
+                where: { basketId: id }
+            });
+
+            return res.json(basketItems);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { deviceId } = req.body;
+
+            if (!deviceId) {
+                throw ApiError.badRequest("Device not found");
+            }
+
+            const deleted = await BasketDevice.destroy({ where: { id: deviceId } });
+            return res.json({ deleted });
+        } catch (e) {
+            next(e);
         }
     }
 }
